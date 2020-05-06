8fae126e3393fffbff4edc8dce95c2de9c4c4367
satoshi kishimoto <kishimoto@forces11.co.jp>
2020-05-06 19:13:02 +0900

    pipでインストールするために最低限必要なファイルを追加

---
+++ python/mayaqt.py
@@ -0,0 +1,17 @@
+from qtpy import QtCore, QtGui, QtWidgets
+from maya.app.general.mayaMixin import MayaQWidgetBaseMixin
+from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
+
+try:
+    from shiboken2 import wrapInstance 
+
+except:
+    from shiboken import wrapInstance 
+
+def get_maya_pointer():
+    from maya import OpenMayaUI
+    return wrapInstance(long(OpenMayaUI.MQtUtil.mainWindow()), QtWidgets.QWidget)
+
+maya_win = get_maya_pointer()
+maya_base_mixin = MayaQWidgetBaseMixin
+maya_dockable_mixin = MayaQWidgetDockableMixin
