0fae2dd20417ff4354017ea02d825801c99073d5
satoshi kishimoto <kishimoto@forces11.co.jp>
2020-05-19 01:41:17 +0900

    汎用的すぎるのでcolors.pyをqt_cud_colors.pyにモジュール名変更

--- python/mayaqt/qt_cud_colors.py
+++ python/mayaqt/qt_cud_colors.py
@@ -4,4 +4,4 @@ from . import *
 accent_colors = [QtGui.QColor(values) for values in cud_colors_.accent_colors]
 base_colors = [QtGui.QColor(values) for values in cud_colors_.base_colors]
 achromatic_colors = [QtGui.QColor(values) for values in cud_colors_.achromatic_colors]
-cud_colors = [QtGui.QColor(values) for values in cud_colors_.all_colors]
+all_colors = [QtGui.QColor(values) for values in cud_colors_.all_colors]
