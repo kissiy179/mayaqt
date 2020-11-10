547868366d05c5cacab2a976568b50c76965d69a
satoshi_kishimoto <kishimoto@forces11.co.jp>
2020-11-10 20:36:48 +0900

    setup.pyを共通のものに変更(nameのみ手書き)

--- setup.py
+++ setup.py
@@ -1,31 +1,23 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
-from os.path import dirname, basename, splitext, exists
 from glob import glob
+from os.path import basename
+from os.path import splitext
+from os.path import exists
 from setuptools import setup
+from setuptools import find_packages
 
 def _requires_from_file(filename):
-    '''requirements.txtから必要パッケージを取得'''
     if not exists(filename):
         return []
     
     return open(filename).read().splitlines()
 
-dir_path = dirname(__file__)
-package_name = basename(dir_path)
-module_names = [splitext(basename(path))[0] for path in glob(r'{}\python\*.py'.format(dir_path))]
-requires_packages = _requires_from_file(r'{}\requirements.txt'.format(dir_path))
-
-# セットアップ
 setup(
-    name=package_name,
+    name='mayaqt',
     version='0.1.0',
     package_dir={"": "python"},
-    py_modules=module_names,
+    py_modules=[splitext(basename(path))[0] for path in glob('python/*.py')],
     include_package_data=True,
     zip_safe=False,
     python_requires=">=2.7",
-    install_requires=[
-    ]
+    install_requires=_requires_from_file('requirements.txt'),
 )
