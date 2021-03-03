ee43ddd2705695813272960d3d2d58970b1d0b62
kissiy179 <kissiy179@gmail.com>
2021-03-04 02:55:06 +0900

    maya_reloadable_menuをrequiremets.txtに追加

--- scripts/test_commands_menu.py
+++ scripts/test_commands_menu.py
@@ -1,7 +1,7 @@
 #encoding: utf-8
 
 import maya.cmds as cmds
-import maya.cmds as cmds
+import reloadable_menu
 
 def show_message(message):
     print(message)
@@ -11,9 +11,10 @@ def show_message(message):
                         # fadeStayTime=3000,
                         )
 
+@reloadable_menu.reloadable_menu('Test Commands')
 def create_menu():
-    cmds.setParent('MayaWindow')
-    cmds.menu(label='Test Commands', tearOff=True)
+    # cmds.setParent('MayaWindow')
+    # cmds.menu(label='Test Commands', tearOff=True)
     # cmds.menuItem(l='test command 1', command='show_message("test command 1")')
     # cmds.menuItem(l='test command 2', command='show_message("test command 2")')
     cmds.menuItem(label='Test Window', command='import test_window; reload(test_window); test_window.show()')
