cbd54fc6a61666ad0ba07f042221847e4e247623
kissiy179 <kissiy179@gmail.com>
2021-03-04 02:48:23 +0900

    Test Commandsメニュー作成コードをuserSetup.pyからtest_commands_menu.pyに分離

---
+++ scripts/test_commands_menu.py
@@ -0,0 +1,19 @@
+#encoding: utf-8
+
+import maya.cmds as cmds
+import maya.cmds as cmds
+
+def show_message(message):
+    print(message)
+    cmds.inViewMessage(assistMessage=message,
+                        position='midCenter',
+                        fade=True,
+                        # fadeStayTime=3000,
+                        )
+
+def create_menu():
+    cmds.setParent('MayaWindow')
+    cmds.menu(label='Test Commands', tearOff=True)
+    # cmds.menuItem(l='test command 1', command='show_message("test command 1")')
+    # cmds.menuItem(l='test command 2', command='show_message("test command 2")')
+    cmds.menuItem(label='Test Window', command='import test_window; reload(test_window); test_window.show()')
--- scripts/userSetup.py
+++ scripts/userSetup.py
@@ -1,23 +1,7 @@
 #encoding: utf-8
 import maya.utils
 import maya.cmds as cmds
-
-def show_message(message):
-    print(message)
-    cmds.inViewMessage(assistMessage=message,
-                        position='midCenter',
-                        fade=True,
-                        # fadeStayTime=3000,
-                        )
-
-
-# メニュー定義
-def create_menu():
-    cmds.setParent('MayaWindow')
-    cmds.menu(label='test commands', tearOff=True)
-    # cmds.menuItem(l='test command 1', command='show_message("test command 1")')
-    # cmds.menuItem(l='test command 2', command='show_message("test command 2")')
-    cmds.menuItem(l='test window', command='import test_window; reload(test_window); test_window.show()')
+import test_commands_menu
 
 # メニュー作成
-maya.utils.executeDeferred(create_menu)
+maya.utils.executeDeferred(test_commands_menu.create_menu)
